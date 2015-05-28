0e41e539836ee5cc4694d4b9717f356267ce8c70
bac29 <bac29@D-173-250-159-110.dhcp4.washington.edu>
2015-05-28 03:16:27 +0000

    test comment dapqa load

--- python/marvin/marvin/static/js/dapqa.js
+++ python/marvin/marvin/static/js/dapqa.js
@@ -256,7 +256,7 @@ function loadComments(key,results) {
     
         // load new comments
         console.log("Loading results for key="+key+"..."); //delete this print (just for debug)
-        for (panelcomment in results.dapqacomments) {
+        results['dapqacomments'].each(function(i,panelcomment) {
         
             //replace this print with actual javascript to replace the panel comments with panelcomment in this loop over results
             //print "Brian, please loadcomment for panelname="+panelcomment['panel']+", panelnumber="+panelcomment['position']+", comment="+panelcomment['comment']+", issueids="+panelcomment['issues']+"!"
