f4b611139af47b19d3540287d9500e0222f0bf65
bac29 <bac29@bandrews.phyast.pitt.edu>
2015-08-04 21:38:53 +0000

    added some basic page error handling

---
+++ python/marvin/marvin/controllers/documentation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import os
+
+import flask
+from flask import request, render_template, send_from_directory, current_app, session as current_session, jsonify,abort
+
+from ..model.database import db
+
+import sdss.internal.database.utah.mangadb.DataModelClasses as datadb
+
+try:
+    from . import valueFromRequest,processRequest
+except ValueError:
+    pass
+    
+    
+doc_page = flask.Blueprint("doc_page", __name__)
+
+@doc_page.route('/documentation.html', methods=['GET'])
+@doc_page.route('/marvin/documentation.html', methods=['GET'])
+def doc():
+    ''' Documentation here. '''
+    
+    session = db.Session() 
+    doc = {}   
+
+    try: return render_template("documentation.html", **doc)
+    except: abort(404) 
+
+@doc_page.errorhandler(404)
+def page_not_found(e):
+    error={}
+    error['page']='Documentation'
+    return render_template('page_not_found.html',**error) 
+
+
