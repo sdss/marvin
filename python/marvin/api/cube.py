c607c72e1ec4117df50d9587743bbe3f974ff5c7
bac29 <bac29@D-173-250-159-110.dhcp4.washington.edu>
2016-02-09 23:05:12 +0000

    starting general api stuff

--- python/marvin/api/cube.py
+++ python/marvin/api/cube.py
@@ -2,6 +2,8 @@ from flask.ext.classy import FlaskView, route
 from marvin.tools.cube import Cube
 import json
 
+''' stuff that runs server-side ''' 
+
 class CubeView(FlaskView):
     route_base='/cubes/'
 
@@ -10,10 +12,10 @@ class CubeView(FlaskView):
 
     def get(self,mangaid):
         cube = Cube(mangaid=mangaid)
-        return 'cube-mangaid: {0},{1},{2}'.format(mangaid,cube.ra,cube.dec)
+        return json.dumps({mangaid: '{0},{1},{2}'.format(mangaid,cube.ra,cube.dec)})
 
     @route('/<mangaid>/spectra/x=<x>/y=<y>/')
     def getSpectra(self,mangaid=None,x=None,y=None):
         cube = Cube(mangaid=mangaid)
         spectrum = cube.getSpectrum(15,15)
-        return json.dumps({'spectrum': 'spectrum: {0},{1} for mangaid {2},{3}'.format(x, y, mangaid, spectrum)})
+        return json.dumps({'data':spectrum})
