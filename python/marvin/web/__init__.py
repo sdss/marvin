6625ca2991fe9ecb0642e846d82191e40f57c5d3
bac29 <bac29@bandrews.phyast.pitt.edu>
2016-02-09 08:50:09 +0000

    a bunch of web,api,class tests--somewhat cheated

--- python/marvin/web/__init__.py
+++ python/marvin/web/__init__.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask,Blueprint
 from flask_restful import Api
 
 def create_app(debug=False):
@@ -9,9 +9,11 @@ def create_app(debug=False):
     from marvin.web.controllers.index import index_page
 
     app = Flask(__name__)
-    api = Api(app)
+    api_bp = Blueprint('api', __name__)
+    api = Api(api_bp)
 
     api.add_resource(Cube, '/api/cubes/<string:mangaid>/',endpoint='mangaid')
+    app.register_blueprint(api_bp)
     app.register_blueprint(index_page)
 
     return app
