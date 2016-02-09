7c73f2933194aee6a2b2370ad7634a1b1bacbad6
bac29 <bac29@bandrews.phyast.pitt.edu>
2016-02-09 21:40:15 +0000

    recent changes

--- python/marvin/__init__.py
+++ python/marvin/__init__.py
@@ -1,6 +1,14 @@
-config = Config()
+import os
 
 class Config:
     def __init__(self):
         self.mode = None
+        self.drpver = None
+        self.dapver = None
+        self.mplver = None
+        self.vermode = None
+        self.download = False
 
+        self.sasurl = os.getenv('SAS_URL') if 'SAS_URL' in os.environ else 'https://sas.sdss.org/'
+
+config = Config()
