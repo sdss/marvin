1068ffa361b38fd997500244462a1e5245eeede3
bandrews <bandrews@D-173-250-159-110.dhcp4.washington.edu>
2016-02-11 00:14:25 +0000

    added logic to handle local vs remote modes

--- python/marvin/__init__.py
+++ python/marvin/__init__.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     def __init__(self):
-        self.mode = None
+        self._mode = 'local'
         self.drpver = None
         self.dapver = None
         self.mplver = None
@@ -11,5 +11,16 @@ class Config:
         self.download = False
 
         self.sasurl = os.getenv('SAS_URL') if 'SAS_URL' in os.environ else 'https://sas.sdss.org/'
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @mode.setter
+    def mode(self, value):
+        if value in ['local', 'remote']:
+            self._mode = value
+        else:
+            raise ValueError('config.mode must be "local" or "remote".')
 
 config = Config()
