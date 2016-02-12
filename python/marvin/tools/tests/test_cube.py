e80daa701a50611e90e319dac11510d0ad0ece76
José Sánchez-Gallego <j.sanchezgallego@uky.edu>
2016-02-12 10:34:51 -0500

    removed all tests (yep, again)

--- python/marvin/tools/tests/test_cube.py
+++ python/marvin/tools/tests/test_cube.py
@@ -101,23 +101,6 @@ class TestCube(unittest.TestCase):
         self._test_getSpectrum(self.cubeFromFile, 3000, expect,
                                ra=232.546383, dec=48.6883954)
 
-    def _getSpectrum_fail(self, x, y, errMsg):
-        cube = Cube(filename=self.filename)
-        with self.assertRaises(AssertionError) as cm:
-            spectrum = cube.getSpectrum(x, y)
-        self.assertIn(errMsg, str(cm.exception))
-
-    def test_getSpectrum_fail_toolargeinput(self):
-        self._getSpectrum_fail(100000, 100000, 'Input x,y coordinates greater than flux dimensions')
-
-    def test_getSpectrum_success(self):
-        cube = Cube(filename=self.filename)
-        spectrum = cube.getSpectrum(0, 0)
-        self.assertIsNotNone(spectrum)
-        self.assertEqual(np.sum(spectrum), 0)
-        spectrum = cube.getSpectrum(15, 15)
-        self.assertIsNotNone(spectrum)
-        self.assertNotEqual(np.sum(spectrum), 0)
 
 if __name__ == '__main__':
     # set to 1 for the usual '...F..' style output, or 2 for more verbose output.
