1e30d1c8929123d3d6c180d4d5dd70e8e89fa117
bac29 <bac29@bandrews.phyast.pitt.edu>
2016-02-13 18:32:39 +0000

    rejiggered the decorators and general utils

--- python/marvin/api/cube.py
+++ python/marvin/api/cube.py
@@ -1,54 +1,45 @@
 from flask.ext.classy import FlaskView, route
 from flask import Blueprint, url_for, current_app
 from marvin.tools.cube import Cube
-from marvin.tools.core import MarvinError
 import json
 import urllib
-from functools import wraps
 from marvin import config
+from marvin.utils.general import parseRoutePath, parseName
+
 config.drpver = 'v1_5_1'  # FIX THIS
 
 ''' stuff that runs server-side '''
 
 api = Blueprint("api", __name__)
 
-
-def parseRoutePath(f):
-    @wraps(f)
-    def decorated_function(inst, *args, **kwargs):
-        for kw in kwargs['path'].split('/'):
-            if len(kw) == 0:
-                continue
-            var, value = kw.split('=')
-            kwargs[var] = value
-        kwargs.pop('path')
-        return f(inst, *args, **kwargs)
-    return decorated_function
-
-
-def parseName(name):
-    try:
-        namesplit = name.split('-')
-    except AttributeError as e:
-        raise AttributeError('Could not split on input name {0}: {1}'.format(name, e))
-
-    if len(namesplit) == 1:
-        raise MarvinError('Input name not of type manga-id or plate-ifu')
-    else:
-        if len(namesplit[0]) == 4:
-            plateifu = name
-            mangaid = None
-        else:
-            mangaid = name
-            plateifu = None
-
-    return mangaid, plateifu
-
 # JSON results to return from API
 result = {'data': None, 'status': -1, 'error': None}
 
 
+def _getCube(name):
+    ''' Retrieve a cube using marvin tools '''
+
+    cube = None
+
+    # parse name into either mangaid or plateifu
+    try:
+        mangaid, plateifu = parseName(name)
+    except Exception as e:
+        result['error'] = 'Failed to parse input name {0}: {1}'.format(name, str(e))
+        return cube
+
+    try:
+        cube = Cube(mangaid=mangaid, plateifu=plateifu, mode='local')
+        result['status'] = 1
+    except Exception as e:
+        result['error'] = 'Failed to retrieve cube {0}: {1}'.format(name, str(e))
+
+    return cube
+
+
 class CubeView(FlaskView):
+    ''' Class describing API calls related to MaNGA Cubes '''
+
     route_base = '/cubes/'
     # decorators = [parseRoutePath]
 
@@ -72,27 +63,9 @@ class CubeView(FlaskView):
 
         return json.dumps(res)
 
-    def _getCube(self, name):
-        cube = None
-
-        # parse name into either mangaid or plateifu
-        try:
-            mangaid, plateifu = parseName(name)
-        except Exception as e:
-            result['error'] = 'Failed to parse input name {0}: {1}'.format(name, str(e))
-            return cube
-
-        try:
-            cube = Cube(mangaid=mangaid, plateifu=plateifu, mode='local')
-            result['status'] = 1
-        except Exception as e:
-            result['error'] = 'Failed to retrieve cube {0}: {1}'.format(name, str(e))
-
-        return cube
-
     def get(self, name):
 
-        cube = self._getCube(name)
+        cube = _getCube(name)
         if cube:
             result['data'] = {name: '{0},{1},{2}'.format(name, cube.plate)}
         return json.dumps(result)
@@ -103,7 +76,7 @@ class CubeView(FlaskView):
     def getSpectra(self, name=None, x=None, y=None, ra=None, dec=None, ext=None):
         # Add ability to grab spectra from fits files
 
-        cube = self._getCube(name)
+        cube = _getCube(name)
         if not cube:
             result['error'] = 'getSpectra: No cube: {0}'.format(result['error'])
             return json.dumps(result)
