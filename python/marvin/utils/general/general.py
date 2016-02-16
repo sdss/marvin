29050258f0f0d4914c62ecb982916bbf91c5ea6b
bac29 <bac29@D-173-250-159-110.dhcp4.washington.edu>
2016-02-16 17:09:10 +0000

    began implementing config setup for versions; encountered circular imports; checking in to have others look at it

--- python/marvin/utils/general/general.py
+++ python/marvin/utils/general/general.py
@@ -1,10 +1,10 @@
-from marvin.tools.core import MarvinError
+from marvin.tools.core.exceptions import MarvinError
 from astropy import wcs
 import numpy as np
 
 # General utilities
 
-__all__ = ['parseName', 'convertCoords']
+__all__ = ['parseName', 'convertCoords', 'lookUpMpl', 'lookUpVersions']
 
 
 def parseName(name):
@@ -45,4 +45,30 @@ def convertCoords(x=None, y=None, ra=None, dec=None, shape=None, hdr=None, mode=
 
     return xCube, yCube
 
+mpldict = {'MPL-4': ('v1_5_1', '1.1.1'), 'MPL-3': ('v1_3_3', 'v1_0_0'), 'MPL-2': ('v1_2_0', None), 'MPL-1': ('v1_0_0', None)}
+
+
+def lookUpVersions(mplver):
+    ''' Retrieve the DRP and DAP versions that make up an MPL '''
+
+    try:
+        drpver, dapver = mpldict[mplver]
+    except KeyError as e:
+        raise MarvinError('MPL version {0} not found in lookup table. Should it be added?  Check for typos..: {1}'.format(mplver, e))
+
+    return drpver, dapver
+
+
+def lookUpMpl(drpver):
+    ''' Retrieve the MPL version for a given DRP version'''
+
+    # Flip the mpldict
+    verdict = {val[0]: key for key, val in mpldict.items()}
+
+    try:
+        mplver = verdict[drpver]
+    except KeyError as e:
+        raise MarvinError('DRP version {0} not found in lookup table. Should it be added?  Check for typos..: {1}'.format(drpver, e))
+
+    return mplver
 
