df463c932a97bfc4048f0cb21a7911a088ea5031
bac29 <bac29@D-173-250-159-110.dhcp4.washington.edu>
2016-02-16 19:53:07 +0000

    added in config info auto passed as params in api interaction request

--- python/marvin/api/api.py
+++ python/marvin/api/api.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import requests
 from marvin import config
+from marvin.tools.core.exceptions import MarvinError
 
 configkeys = ['mode', 'mplver', 'drpver', 'dapver']
 
@@ -12,16 +13,19 @@ class Interaction(object):
     def __init__(self, route, params=None, request_type='get'):
         self.results = None
         self.route = route
-        self.url = os.path.join(config.sasurl, route)
         self.params = params
         self.statuscodes = {200: 'Ok', 401: 'Authentication Required', 404: 'URL Not Found', 500: 'Internal Server Error', 405: 'Method Not Allowed', 400: 'Bad Request'}
-        self._sendRequest(request_type)
+        self.url = os.path.join(config.sasurl, route) if self.route else None
+        if self.url:
+            self._sendRequest(request_type)
+        else:
+            raise MarvinError('No route and/or url specified {0}'.format(self.url))
 
     def _checkResponse(self, response):
         if response.status_code == 200:
             try:
                 self.results = response.json()
-            except Exception as e:
+            except ValueError as e:
                 self.results = response.text
                 raise RuntimeError('Response not in JSON format. {0} {1}'.format(e, self.results))
         else:
@@ -29,7 +33,11 @@ class Interaction(object):
             self.results = {'http status code': response.status_code, 'message': errmsg}
 
     def _sendRequest(self, request_type):
+
         assert request_type in ['get', 'post'], 'Valid request types are "get" and "post".'
+
+        self._loadConfigParams()
+
         if request_type == 'get':
             r = requests.get(self.url, params=self.params)
         elif request_type == 'post':
@@ -54,4 +62,12 @@ class Interaction(object):
             self.results[key] = config.__getattribute__(key)
 
     def checkConfig(self):
-        return {k: self.results[k] for k in configkeys}
+        return {k: self.results[k] if k in self.results else '' for k in configkeys}
+
+    def _loadConfigParams(self):
+        if self.params:
+            for k in configkeys:
+                self.params[k] = config.__getattribute__(k)
+        else:
+            self.params = {k: config.__getattribute__(k) for k in configkeys}
+
