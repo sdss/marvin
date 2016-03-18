76725fca56a98e71dde6bd98fbd09f34e7e41758
bandrews <bandrews@D-173-250-159-110.dhcp4.washington.edu>
2016-03-18 21:25:10 +0000

    added method to post a query via the API and return a result

--- python/marvin/api/query.py
+++ python/marvin/api/query.py
@@ -1,24 +1,33 @@
+import json
+from flask import request
 from flask.ext.classy import route
 from marvin.api.base import BaseView
-import json
+
+
+def _getCubes(query):
+    """Run query locally at Utah."""
+    # results = do_query(query)
+    results = {'data': 'jackpot!'}
+    return results
 
 
 class QueryView(BaseView):
     """Class describing API calls related to queries."""
 
-    route_base = '/queries/'
+    route_base = '/query/'
+    # config.mode = 'remote'
 
     def index(self):
         self.results['data'] = 'this is a query!'
         return json.dumps(self.results)
 
-    # @route('/plateifus/')
-    # def get_plateifus(self, name):
-    #     """This method performs a get request at the url route /queries/<id>"""
-    #     cube, res = _getCube(name)
-    #     self.update_results(res)
-    #     if cube:
-    #         self.results['data'] = {name: '{0},{1},{2},{3}'.format(
-    #                                     name, cube.plate, cube.ra, cube.dec)}
-    #     return json.dumps(self.results)
+    """example query post:
+    curl -i -H "Content-Type: application/json" -X POST -d '{"query":"nsa_redshift=0.01-0.015"}' http://localhost:5000/api/query/cubes/
+    """
 
+    @route('/cubes/', methods=['POST'])
+    def cube_query(self):
+        self.results['query'] = request.json['query']
+        res = _getCubes(self.results['query'])
+        self.update_results(res)
+        return json.dumps(self.results)
