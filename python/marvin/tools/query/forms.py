b0035ff7eb3b06c4615de11dac7dc6659f36a19b
bac29 <bac29@D-173-250-159-110.dhcp4.washington.edu>
2016-03-23 23:10:39 +0000

    added operand validator on mainform marvinweb search box

--- python/marvin/tools/query/forms.py
+++ python/marvin/tools/query/forms.py
@@ -111,7 +111,9 @@ class ParamFormLookupDict(dict):
 
 class MainForm(Form):
     ''' Main Level WTForm for Marvin '''
-    searchbox = StringField('Search', [validators.Length(min=3, message='Input must have at least 3 characters'), validators.DataRequired(message='Input filter string required')])
+    searchbox = StringField('Search', [validators.Length(min=3, message='Input must have at least 3 characters'),
+                            validators.DataRequired(message='Input filter string required'),
+                            validators.AnyOf(opdict.values(), message='Input must contain a valid operand')])
 
 
 class MarvinForm(object):
