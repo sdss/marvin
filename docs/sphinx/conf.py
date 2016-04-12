8bd8b686de6e436f8d979205be57957d54b12dce
bac29 <bac29@bandrews.phyast.pitt.edu>
2016-04-12 02:13:48 +0000

    added some extensions

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -32,9 +32,11 @@ sys.path.insert(0, os.path.abspath('../../python/'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+    'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.todo',
+    'sphinx.ext.viewcode','sphinx.ext.mathjax',
+    'matplotlib.sphinxext.only_directives',
+    'matplotlib.sphinxext.plot_directive',
 ]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
