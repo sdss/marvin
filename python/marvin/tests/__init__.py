8a242bad030af0c643cd785131cd5599442f5f57
bac29 <bac29@D-173-250-159-110.dhcp4.washington.edu>
2016-04-19 20:40:05 +0000

    refactored testing directories

--- python/marvin/tests/__init__.py
+++ python/marvin/tests/__init__.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from unittest import TestCase
+import warnings
+from marvin.tools.core.exceptions import MarvinSkippedTestWargning
+from functools import wraps
+
+
+# Moved from init in utils/test
+
 # Copied from http://bit.ly/20i4EHC
 
 from functools import wraps
@@ -45,3 +56,25 @@ class TemplateTestCase(type):
 
 def Call(*args, **kwargs):
     return (args, kwargs)
+
+# Copied from init in tools/test
+
+
+# Decorator to skip a test if the session is None (i.e., if there is no DB)
+def skipIfNoDB(test):
+    @wraps(test)
+    def wrapper(self, *args, **kwargs):
+        if not self.session:
+            return self.skipTest(test)
+        else:
+            return test(self, *args, **kwargs)
+    return wrapper
+
+
+class MarvinTest(TestCase):
+    """Custom class for Marvin-tools tests."""
+
+    def skipTest(self, test):
+        """Issues a warning when we skip a test."""
+        warnings.warn('Skipped test {0} because there is no DB connection.'
+                      .format(test.__name__), MarvinSkippedTestWargning)
