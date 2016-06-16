2b1970da804ee25fa71fd2ecc380f597188437ce
bac29 <bac29@D-173-250-159-110.dhcp4.washington.edu>
2016-06-16 23:01:27 +0000

    changing marvin_refactor to marvin2 uwsgi files in marvin

--- python/marvin/web/__init__.py
+++ python/marvin/web/__init__.py
@@ -107,7 +107,7 @@ def create_app(debug=False):
     os.environ['SAS_SANDBOX'] = 'sas/mangawork/manga/sandbox'
     release = os.environ.get('MARVIN_RELEASE', 'mangawork')
     os.environ['SAS_PREFIX'] = 'marvin2' if release == 'mangawork' else 'dr13/marvin'
-    url_prefix = '/marvin2' if local else '/{0}'.format(os.environ['SAS_PREFIX'])
+    url_prefix = '/marvin' if local else '/{0}'.format(os.environ['SAS_PREFIX'])
 
     # ----------------------------------
     # Load the appropriate Flask configuration file for debug or production
