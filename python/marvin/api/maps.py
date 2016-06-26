ba4ba52804492b67a76975d48ab054f733357b92
José Sánchez-Gallego <gallegoj@uw.edu>
2016-06-26 16:21:38 -0700

    moved Maps load to independent function

--- python/marvin/api/maps.py
+++ python/marvin/api/maps.py
@@ -20,6 +20,41 @@ import marvin.tools.maps
 import marvin.utils.general
 
 
+def _getMaps(name, **kwargs):
+    """Returns a Maps object after parsing the name."""
+
+    results = {}
+
+    # Makes sure we don't use the wrong mode.
+    kwargs.pop('mode', None)
+
+    # Parses name into either mangaid or plateifu
+    try:
+        idtype = marvin.utils.general.parseIdentifier(name)
+    except Exception as ee:
+        results['error'] = 'Failed to parse input name {0}: {1}'.format(name, str(ee))
+        return None, results
+
+    plateifu = None
+    mangaid = None
+    try:
+        if idtype == 'plateifu':
+            plateifu = name
+        elif idtype == 'mangaid':
+            mangaid = name
+        else:
+            raise marvin.core.exceptions.MarvinError(
+                'invalid plateifu or mangaid: {0}'.format(idtype))
+
+        maps = marvin.tools.maps.Maps(mangaid=mangaid, plateifu=plateifu,
+                                      mode='local', **kwargs)
+        results['status'] = 1
+    except Exception as ee:
+        results['error'] = 'Failed to retrieve maps {0}: {1}'.format(name, str(ee))
+
+    return maps, results
+
+
 class MapsView(marvin.api.base.BaseView):
     """Class describing API calls related to MaNGA Maps."""
 
@@ -43,34 +78,11 @@ class MapsView(marvin.api.base.BaseView):
 
         """
 
-        results = {}
-
-        # Parses name into either mangaid or plateifu
-        try:
-            idtype = marvin.utils.general.parseIdentifier(name)
-        except Exception as ee:
-            results['error'] = 'Failed to parse input name {0}: {1}'.format(name, str(ee))
-            self.update_results(results)
-            return json.dumps(self.results)
-
-        plateifu = None
-        mangaid = None
-        try:
-            if idtype == 'plateifu':
-                plateifu = name
-            elif idtype == 'mangaid':
-                mangaid = name
-            else:
-                raise marvin.core.exceptions.MarvinError(
-                    'invalid plateifu or mangaid: {0}'.format(idtype))
-
-            maps = marvin.tools.maps.Maps(mangaid=mangaid, plateifu=plateifu,
-                                          mode='local')
-            results['status'] = 1
-        except Exception as ee:
-            results['error'] = 'Failed to retrieve maps {0}: {1}'.format(name, str(ee))
-
+        maps, results = _getMaps(name)
         self.update_results(results)
+
+        if maps is None:
+            return json.dumps(self.results)
 
         wcs_header = maps.data.cube.wcs.makeHeader().tostring()
         shape = maps.shape
