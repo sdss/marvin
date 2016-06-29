e272d43d5850016f9fbb1412fc8b591ec0e37ce9
bac29 <bac29@bandrews.phyast.pitt.edu>
2016-06-29 05:58:57 +0000

    implemented pagination in remote mode queries using getNext,getPrevious,getSubset; added switchSasUrl convenience fxn

--- python/marvin/api/query.py
+++ python/marvin/api/query.py
@@ -6,16 +6,20 @@ from marvin.tools.query import doQuery, Query
 from marvin.core import MarvinError
 
 
-def _getCubes(searchfilter, params=None):
+def _getCubes(searchfilter, params=None, start=None, end=None):
     """Run query locally at Utah."""
 
     q, r = doQuery(searchfilter=searchfilter, returnparams=params, mode='local')
-    r.getAll()
-    print('query worked', r.results[0])
-    output = dict(data=r.results, query=r.showQuery(),
+    results = r.results
+
+    # get a subset
+    if start:
+        chunk = int(end)-int(start)
+        results = r.getSubset(int(start), limit=chunk)
+
+    output = dict(data=results, query=r.showQuery(),
                   filter=searchfilter, params=params,
-                  queryparams_order=q.queryparams_order)
-    print('query api', output)
+                  queryparams_order=q.queryparams_order, count=len(results), totalcount=r.count)
     return output
 
 
@@ -28,6 +32,7 @@ class QueryView(BrainQueryView):
 
     @route('/cubes/', methods=['GET', 'POST'], endpoint='querycubes')
     def cube_query(self):
+        ''' do a remote query '''
         searchfilter = self.results['inconfig'].get('searchfilter', None)
         params = self.results['inconfig'].get('params', None)
         print('cube_query', searchfilter, params)
@@ -38,7 +43,23 @@ class QueryView(BrainQueryView):
         else:
             self.results['status'] = 1
             self.update_results(res)
-        print('about to return', self.results)
+
+        return json.dumps(self.results)
+
+    @route('/cubes/getsubset', methods=['GET', 'POST'], endpoint='getsubset')
+    def query_getsubset(self):
+        ''' remotely grab a subset of values '''
+        searchfilter = self.results['inconfig'].get('searchfilter', None)
+        params = self.results['inconfig'].get('params', None)
+        start = self.results['inconfig'].get('start', None)
+        end = self.results['inconfig'].get('end', None)
+        try:
+            res = _getCubes(searchfilter, params=params, start=int(start), end=int(end))
+        except MarvinError as e:
+            self.results['error'] = str(e)
+        else:
+            self.results['status'] = 1
+            self.update_results(res)
 
         return json.dumps(self.results)
 
