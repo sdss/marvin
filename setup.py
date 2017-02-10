ad854fb908b9a1dd43f59afae340e8f00943fb09
Brian Cherinka <havok2063@hotmail.com>
2017-02-10 00:27:15 -0500

    added bugfix for galaxy page on web when galaxy has no nsa info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # @Author: Brian Cherinka
 # @Date:   2016-10-19 17:36:00
 # @Last modified by:   Brian Cherinka
-# @Last Modified time: 2017-01-31 13:36:17
+# @Last Modified time: 2017-02-09 13:08:40
 #
 # This is the Marvin setup
 #
@@ -56,7 +56,7 @@ install_requires = [line.strip().replace('==', '>=') for line in open(requiremen
                     if not line.strip().startswith('#') and line.strip() != '']
 
 NAME = 'sdss-marvin'
-VERSION = '2.1.0dev'
+VERSION = '2.1.0'
 RELEASE = 'dev' not in VERSION
 generate_version_py(NAME, VERSION, RELEASE)
 
