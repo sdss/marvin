4067cef51560db001e8b1e449340363d2f94cc7b
Brian Cherinka <havok2063@hotmail.com>
2017-03-17 10:46:53 -0400

    setting version 2.1.2 for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # @Author: Brian Cherinka
 # @Date:   2016-10-19 17:36:00
 # @Last modified by:   Brian Cherinka
-# @Last Modified time: 2017-03-16 10:30:30
+# @Last Modified time: 2017-03-17 10:46:36
 #
 # This is the Marvin setup
 #
@@ -68,7 +68,7 @@ install_requires = [line.strip().replace('==', '>=') for line in open(requiremen
 
 NAME = 'sdss-marvin'
 # do not use x.x.x-devx.  things complain.  instead use x.x.xdevx
-VERSION = '2.1.2dev3'
+VERSION = '2.1.2'
 RELEASE = 'dev' not in VERSION
 generate_version_py(NAME, VERSION, RELEASE)
 
