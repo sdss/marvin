b21d7d44bbbbfbd0e6306272f81e88cd7bb10445
Brett Andrews <brett.h.andrews@gmail.com>
2017-03-22 11:36:13 -0400

    removes comments and reorganizes conftest

--- python/marvin/tests/conftest.py
+++ python/marvin/tests/conftest.py
@@ -11,99 +11,13 @@ import os
 import pytest
 
 from marvin import config, marvindb
-from marvin.api.api import Interaction
 from marvin.tools.maps import _get_bintemps
 
-
-class MarvinSession:
-    """Custom class for Marvin-tools tests."""
-
-    # necessary?
-    # def __init__(self, request):
-    #     self.request = request
-
-    # TODO: Replace instance of skipTest and skipBrian with pytest's skipif
-    #
-    # def skipTest(self, test):
-    #     """Issues a warning when we skip a test."""
-    #     warnings.warn('Skipped test {0} because there is no DB connection.'
-    #                   .format(test.__name__), MarvinSkippedTestWarning)
-    #
-    # def skipBrian(self, test):
-    #     """Issues a warning when we skip a test."""
-    #     warnings.warn('Skipped test {0} because there is no Brian.'
-    #                   .format(test.__name__), MarvinSkippedTestWarning)
-
-    # TODO how to pass in classmethod without subclassing?
-    # Can I do this as a session level fixture? Probably
-    @classmethod
-    def set_config(cls):
-        config.use_sentry = False
-        config.add_github_message = False
-
-        # TODO use monkeypatch to safely set these attributes
-        #
-        # set initial config variables
-        # cls.init_mode = config.mode
-        # cls.init_sasurl = config.sasurl
-        # cls.init_urlmap = config.urlmap
-        # cls.init_xyorig = config.xyorig
-        # cls.init_traceback = config._traceback
-        # cls.init_keys = ['mode', 'sasurl', 'urlmap', 'xyorig', 'traceback']
-
-    def set_db(self):
-        self._marvindb = marvindb
-        self.session = marvindb.session
-
-    # def set_paths(self):
-        # self.sasbasedir = os.getenv("$SAS_BASE_DIR")
-        # self.mangaredux = os.getenv("MANGA_SPECTRO_REDUX")
-        # self.mangaanalysis = os.getenv("MANGA_SPECTRO_ANALYSIS")
-
-        # TODO refactor into separate method (setup_8485_1901)
-        #
-        # testing data for 8485-1901
-        # cls.set_plateifu(plateifu='8485-1901')
-        # cls.mangaid = '1-209232'
-        # cls.cubepk = 10179
-        # cls.ra = 232.544703894
-        # cls.dec = 48.6902009334
-        # cls.redshift = 0.0407447
-        # cls.dir3d = 'stack'
-        # TODO add release setup session fixture
-        # self.release = 'MPL-5'
-        # self.drpver, self.dapver = config.lookUpVersions(self.release)
-        # TODO add filename setup session fixture
-        # cls.bintemp = _get_bintemps(cls.dapver, default=True)
-        # cls.defaultbin, cls.defaulttemp = cls.bintemp.split('-', 1)
-        # cls.cubename = 'manga-{0}-LOGCUBE.fits.gz'.format(cls.plateifu)
-        # cls.rssname = 'manga-{0}-LOGRSS.fits.gz'.format(cls.plateifu)
-        # cls.imgname = '{0}.png'.format(cls.ifu)
-        # cls.mapsname = 'manga-{0}-MAPS-{1}.fits.gz'.format(cls.plateifu, cls.bintemp)
-        # cls.modelname = 'manga-{0}-LOGCUBE-{1}.fits.gz'.format(cls.plateifu, cls.bintemp)
-
-    # TODO replace with monkeypatch
-    #
-    # def _reset_the_config(self):
-    #     keys = self.init_keys
-    #     for key in keys:
-    #         ikey = 'init_{0}'.format(key)
-    #         if hasattr(self, ikey):
-    #             k = '_{0}'.format(key) if 'traceback' in key else key
-    #             config.__setattr__(k, self.__getattribute__(ikey))
-
-    @classmethod
-    def set_sasurl(cls, loc='local', port=5000):
-        istest = True if loc == 'utah' else False
-        config.switchSasUrl(loc, test=istest, port=port)
-        response = Interaction('api/general/getroutemap', request_type='get')
-        config.urlmap = response.getRouteMap()
-
-    # TODO What is the best way to test multiple releases (i.e., parameterization)?
-    # We need multiple parameterization with releases and galaxies
-    def _update_release(self, release):
-        config.setMPL(release)
-        self.drpver, self.dapver = config.lookUpVersions(release=release)
+# class MarvinTest
+# TODO Replace skipTest and skipBrian with skipif
+# TODO use monkeypatch to set initial config variables
+# TODO replace _reset_the_config with monkeypatch
+# TODO reimplement set_sasurl (use function-level fixture?)
 
 
 class Galaxy:
@@ -171,14 +85,28 @@ class Galaxy:
         self.modelpath = os.path.join(self.analysispath, self.modelname)
 
 
+class DB:
+    def __init__(self):
+        self._marvindb = marvindb
+        self.session = marvindb.session
+
+
+@pytest.fixture(scope='session')
+def set_config():
+    config.use_sentry = False
+    config.add_github_message = False
+
+
+@pytest.fixture(scope='function')
+def _update_release(self, release):
+    config.setMPL(release)
+    self.drpver, self.dapver = config.lookUpVersions(release=release)
+
+
 @pytest.fixtures(scope='session')
-def start_marvin_session():
-    # TODO how do I access MarvinSession classmethods since it's no longer subclassed from
-    # can I access `ms`?
-    with MarvinSession() as ms:
-        ms.set_config()
-        ms.set_db()
-        yield ms
+def start_marvin_session(set_config):
+    with DB() as db:
+        yield db
 
 
 @pytest.fixtures(scope='session')
