97adb513131070973bbff596143cb0cb9b517dc4
Brett Andrews <brett.h.andrews@gmail.com>
2017-05-03 22:37:47 -0400

    adds convenience plotting method back to Map

--- python/marvin/tools/map.py
+++ python/marvin/tools/map.py
@@ -7,33 +7,6 @@
 # map.py
 #
 # Created by José Sánchez-Gallego on 26 Jun 2016.
-#
-# Includes code from mangadap.plot.maps.py licensed under the following 3-clause BSD license.
-#
-# Copyright (c) 2015, SDSS-IV/MaNGA Pipeline Group
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, are permitted
-# provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this list of conditions
-# and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of
-# conditions and the following disclaimer in the documentation and/or other materials provided with
-# the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its contributors may be used to
-# endorse or promote products derived from this software without specific prior written permission.
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 from __future__ import division
@@ -55,7 +28,7 @@ import marvin.api.api
 import marvin.core.marvin_pickle
 import marvin.core.exceptions
 import marvin.tools.maps
-import marvin.utils.plot.colorbar as colorbar
+import marvin.utils.plot.map
 
 
 try:
@@ -289,3 +262,10 @@ class Map(object):
         """
 
         return marvin.core.marvin_pickle.restore(path, delete=delete)
+
+    def plot(self, *args, **kwargs):
+        """Convenience method for map plotting that wraps marvin.utils.plot.map.plot.
+        
+        See marvin.utils.plot.map.plot for full documentation.
+        """
+        return marvin.utils.plot.map.plot(dapmap=self, *args, **kwargs)
