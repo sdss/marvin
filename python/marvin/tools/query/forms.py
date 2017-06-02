0c435604aa72cbdb7cac466c192f5d35e57fa7da
Brian Cherinka <havok2063@hotmail.com>
2017-06-01 23:44:23 -0600

    fixing bug with link in search and adding other link to input search box

--- python/marvin/tools/query/forms.py
+++ python/marvin/tools/query/forms.py
@@ -294,7 +294,9 @@ class ValidOperand(object):
 
 class SearchForm(Form):
     ''' Main Search Level WTForm for Marvin '''
-    searchbox = StringField('Input Search Filter', [validators.Length(min=3, message='Input must have at least 3 characters'),
+
+    searchbox = StringField("<a target='_blank' href='https://sdss-marvin.readthedocs.io/en/stable/tools/query/query_using.html'>Input Search Filter</a>",
+        [validators.Length(min=3, message='Input must have at least 3 characters'),
                             validators.DataRequired(message='Input filter string required'),
                             ValidOperand('[<>=]', message='Input must contain a valid operand.')])
     returnparams = SelectMultipleField("<a target='_blank' href='https://api.sdss.org/doc/manga/marvin/query_params.html'>Return Parameters</a>")
