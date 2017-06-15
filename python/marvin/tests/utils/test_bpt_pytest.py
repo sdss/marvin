9235e7ff57d54d594a3377f807fe98e3f325f41a
Brett Andrews <brett.h.andrews@gmail.com>
2017-06-15 15:15:34 -0400

    performs initial unittest2pytest conversion of test_bpt

--- python/marvin/tests/utils/test_bpt_pytest.py
+++ python/marvin/tests/utils/test_bpt_pytest.py
@@ -10,13 +10,13 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-
 import os
 import unittest
 import warnings
 
 from matplotlib import pyplot as plt
 import numpy as np
+import pytest
 
 from marvin.tests import MarvinTest
 from marvin.tools.maps import Maps
@@ -42,21 +42,21 @@ class TestBPT(MarvinTest):
     def _run_tests_8485_1901(self, maps):
 
         masks, figure = maps.get_bpt(show_plot=False, return_figure=True, use_oi=True)
-        self.assertIsInstance(figure, plt.Figure)
+        assert isinstance(figure, plt.Figure)
 
         for em_mech in self.emission_mechanisms:
-            self.assertIn(em_mech, masks.keys())
+            assert em_mech in masks.keys()
 
-        self.assertEqual(np.sum(masks['sf']['global']), 62)
-        self.assertEqual(np.sum(masks['comp']['global']), 1)
+        assert np.sum(masks['sf']['global']) == 62
+        assert np.sum(masks['comp']['global']) == 1
 
         for em_mech in ['agn', 'seyfert', 'liner']:
-            self.assertEqual(np.sum(masks[em_mech]['global']), 0)
+            assert np.sum(masks[em_mech]['global']) == 0
 
-        self.assertEqual(np.sum(masks['ambiguous']['global']), 8)
-        self.assertEqual(np.sum(masks['invalid']['global']), 1085)
+        assert np.sum(masks['ambiguous']['global']) == 8
+        assert np.sum(masks['invalid']['global']) == 1085
 
-        self.assertEqual(np.sum(masks['sf']['sii']), 176)
+        assert np.sum(masks['sf']['sii']) == 176
 
     def test_8485_1901_bpt_file(self):
 
@@ -77,22 +77,22 @@ class TestBPT(MarvinTest):
 
         maps = Maps(plateifu=self.plateifu)
         masks, figure = maps.get_bpt(show_plot=False, return_figure=True, use_oi=False)
-        self.assertIsInstance(figure, plt.Figure)
+        assert isinstance(figure, plt.Figure)
 
         for em_mech in self.emission_mechanisms:
-            self.assertIn(em_mech, masks.keys())
+            assert em_mech in masks.keys()
 
-        self.assertNotIn('oi', masks['sf'].keys())
+        assert 'oi' not in masks['sf'].keys()
 
-        self.assertEqual(np.sum(masks['sf']['global']), 149)
-        self.assertEqual(np.sum(masks['sf']['sii']), 176)
+        assert np.sum(masks['sf']['global']) == 149
+        assert np.sum(masks['sf']['sii']) == 176
 
     def test_8485_1901_bpt_no_figure(self):
 
         maps = Maps(plateifu=self.plateifu)
         bpt_return = maps.get_bpt(show_plot=False, return_figure=False, use_oi=False)
 
-        self.assertIsInstance(bpt_return, dict)
+        assert isinstance(bpt_return, dict)
 
     def test_8485_1901_bpt_snr_min(self):
 
@@ -100,10 +100,10 @@ class TestBPT(MarvinTest):
         masks = maps.get_bpt(snr_min=5, return_figure=False, show_plot=False)
 
         for em_mech in self.emission_mechanisms:
-            self.assertIn(em_mech, masks.keys())
+            assert em_mech in masks.keys()
 
-        self.assertEqual(np.sum(masks['sf']['global']), 28)
-        self.assertEqual(np.sum(masks['sf']['sii']), 112)
+        assert np.sum(masks['sf']['global']) == 28
+        assert np.sum(masks['sf']['sii']) == 112
 
     def test_8485_1901_bpt_snr_deprecated(self):
 
@@ -112,16 +112,16 @@ class TestBPT(MarvinTest):
         with warnings.catch_warnings(record=True) as warning_list:
             masks = maps.get_bpt(snr=5, return_figure=False, show_plot=False)
 
-        self.assertTrue(len(warning_list) == 1)
-        self.assertEqual(str(warning_list[0].message),
-                         'snr is deprecated. Use snr_min instead. '
-                         'snr will be removed in a future version of marvin')
+        assert len(warning_list) == 1
+        assert str(warning_list[0].message) == \
+                         'snr is deprecated. Use snr_min instead. ' \
+                         'snr will be removed in a future version of marvin'
 
         for em_mech in self.emission_mechanisms:
-            self.assertIn(em_mech, masks.keys())
+            assert em_mech in masks.keys()
 
-        self.assertEqual(np.sum(masks['sf']['global']), 28)
-        self.assertEqual(np.sum(masks['sf']['sii']), 112)
+        assert np.sum(masks['sf']['global']) == 28
+        assert np.sum(masks['sf']['sii']) == 112
 
 
 if __name__ == '__main__':
