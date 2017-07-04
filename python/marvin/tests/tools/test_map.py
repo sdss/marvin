8732cca0227b3608f9935eabcff571f1db54dc19
Brett Andrews <brett.h.andrews@gmail.com>
2017-07-04 15:56:25 -0400

    adds OIII vel and stellar sigma to maps tests

--- python/marvin/tests/tools/test_map.py
+++ python/marvin/tests/tools/test_map.py
@@ -25,8 +25,11 @@ def _get_maps_kwargs(galaxy, data_origin):
 
     return maps_kwargs
 
+
 @pytest.fixture(scope='function', params=[('emline_gflux', 'ha_6564'),
-                                          ('stellar_vel', None)])
+                                          ('emline_gvel', 'oiii_5008'),
+                                          ('stellar_vel', None),
+                                          ('stellar_sigma', None)])
 def map_(request, galaxy, data_origin):
 
     # TODO Remove
@@ -57,12 +60,11 @@ def map_(request, galaxy, data_origin):
     map_ = maps.getMap(property_name=request.param[0], channel=request.param[1])
     return map_
 
-    
 
 class TestMap(object):
-    
+
     def test_map(self, map_, galaxy):
-        
+
         assert map_.release == galaxy.release
 
         assert tuple(map_.shape) == tuple(galaxy.shape)
@@ -72,13 +74,12 @@ class TestMap(object):
 
         assert (map_.masked.data == map_.value).all()
         assert (map_.masked.mask == map_.mask.astype(bool)).all()
-        
+
         assert pytest.approx(map_.snr, np.abs(map_.value * np.sqrt(map_.ivar)))
-        
+
         assert map_.header['BUNIT'] == map_.unit
 
         assert isinstance(map_.header, astropy.io.fits.header.Header)
-
 
     def test_plot(self, map_):
 
@@ -86,16 +87,15 @@ class TestMap(object):
         assert isinstance(fig, matplotlib.figure.Figure)
         assert isinstance(ax, matplotlib.axes._subplots.Subplot)
 
-
     def test_save_and_restore(self, temp_scratch, map_):
-        
+
         fout = temp_scratch.join('test_map.mpf')
         map_.save(str(fout))
         assert fout.check() is True
-        
+
         map_restored = Map.restore(str(fout), delete=False)
         assert tuple(map_.shape) == tuple(map_restored.shape)
 
 
 # extend the number of property name + channel combinations
-        
+
