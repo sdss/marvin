33602e010a93baa66200f1e3d1df197d2a661271
José Sánchez-Gallego <gallegoj@uw.edu>
2017-07-31 13:47:03 -0400

    ignoring deprecation warning

--- python/marvin/__init__.py
+++ python/marvin/__init__.py
@@ -46,6 +46,7 @@ log = initLog(logFilePath)
 warnings.simplefilter('once')
 warnings.filterwarnings('ignore', 'Skipped unsupported reflection of expression-based index')
 warnings.filterwarnings('ignore', '(.)+size changed, may indicate binary incompatibility(.)+')
+warnings.filterwarnings('ignore', '(.)+use of getfuncargvalue is deprecated,(.)+')
 warnings.filterwarnings('ignore', category=FITSFixedWarning)
 
 # Filters for PY3
