1f3a1cb3c97fd5243c35f3a30c4805593cde8bac
José Sánchez-Gallego <gallegoj@uw.edu>
2017-08-09 01:32:19 -0700

    Map is not a subclass from Quantity

--- python/marvin/tools/map.py
+++ python/marvin/tools/map.py
@@ -18,6 +18,8 @@ import os
 import warnings
 
 from astropy.io import fits
+from astropy.units import Quantity
+
 import numpy as np
 
 import marvin
@@ -35,7 +37,7 @@ except ImportError:
     sqlalchemy = None
 
 
-class Map(object):
+class Map(Quantity):
     """Describes a single DAP map in a Maps object.
 
     Unlike a ``Maps`` object, which contains all the information from a DAP
@@ -44,9 +46,8 @@ class Map(object):
     channels. A ``Map`` would be, for example, the map for ``emline_gflux`` and
     channel ``ha_6564``.
 
-    A ``Map`` is basically a set of three Numpy 2D arrays (``value``, ``ivar``,
-    and ``mask``), with extra information and additional methods for
-    functionality.
+    A ``Map`` returns and astropy 2D Quantity-like array with additional
+    attributes for ``ivar`` and ``mask``.
 
     ``Map`` objects are not intended to be initialised directly, at least
     for now. To get a ``Map`` instance, use the
@@ -66,52 +67,81 @@ class Map(object):
 
     """
 
-    def __init__(self, maps, property_name, channel=None):
+    def __new__(cls, maps, property_name, channel=None, dtype=None, copy=True):
 
         assert isinstance(maps, marvin.tools.maps.Maps)
 
-        self.maps = maps
-        self.datamodel = maps.datamodel
+        maps = maps
+        datamodel = maps.datamodel
 
         if isinstance(property_name, Property):
-            self.property = property_name
+            prop = property_name
         else:
-            self.property = self.maps._match_properties(property_name, channel=channel)
+            prop = maps._match_properties(property_name, channel=channel)
 
-        assert self.property in self.datamodel.properties, \
+        assert prop in datamodel.properties, \
             'failed sanity check. Property does not match.'
 
-        self.channel = self.property.channel
-        self.shape = self.maps.shape
+        channel = prop.channel
 
-        self.release = maps.release
-
-        self.value = None
-        self.ivar = None
-        self.mask = None
-
-        self.header = None
-        self.unit = None
+        release = maps.release
 
         if maps.data_origin == 'file':
-            self._load_map_from_file()
+            value, ivar, mask, header = cls._get_from_file(maps, prop)
         elif maps.data_origin == 'db':
-            self._load_map_from_db()
+            value, ivar, mask, header = cls._get_from_db(maps, prop)
         elif maps.data_origin == 'api':
-            self._load_map_from_api()
+            value, ivar, mask, header = cls._get_from_api(maps, prop)
 
-        self.unit = self.property.unit
-        self.scale = self.property.scale
+        unit = prop.unit
+        value = value * prop.scale
 
-        # TODO: this may be a too aggresive masking. When the maskbits are better implemented
-        # we should use the DONOTUSE mask.
-        self.value = np.ma.array(self.value,
-                                 mask=(self.mask > 0 if self.mask is not None else False))
+        obj = super(Map, cls).__new__(cls, value, unit=unit, dtype=dtype, copy=copy)
 
-    def __repr__(self):
+        obj.property = prop
+        obj.channel = channel
 
-        return ('<Marvin Map (plateifu={0.maps.plateifu!r}, property={0.property.name!r}, '
-                'channel={0.channel!r})>'.format(self))
+        obj.maps = maps
+        obj.release = release
+        obj.datamodel = datamodel
+
+        obj.ivar = (np.array(ivar) / (prop.scale ** 2)) if ivar is not None else None
+        obj.mask = np.array(mask) if mask is not None else None
+
+        return obj
+
+    def __array_finalize__(self, obj):
+
+        if obj is None:
+            return
+
+        self.property = getattr(obj, 'property', None)
+        self.channel = getattr(obj, 'channel', None)
+        self.maps = getattr(obj, 'maps', None)
+        self.release = getattr(obj, 'release', None)
+        self.datamodel = getattr(obj, 'datamodel', None)
+
+        self.ivar = getattr(obj, 'ivar', None)
+        self.mask = getattr(obj, 'mask', None)
+
+    @property
+    def error(self):
+        """The standard deviation of the measurement."""
+
+        if self.ivar is None:
+            return None
+
+        np.seterr(divide='ignore')
+
+        return np.sqrt(1. / self.ivar) * self.unit
+
+    @property
+    def masked(self):
+        """Returns a masked array."""
+
+        assert self.masks is not None, 'mask is None'
+
+        return np.ma.array(self.value, mask=self.mask > 0)
 
     @property
     def snr(self):
@@ -119,28 +149,30 @@ class Map(object):
 
         return np.abs(self.value * np.sqrt(self.ivar))
 
-    def _load_map_from_file(self):
+    @staticmethod
+    def _get_from_file(maps, prop):
         """Initialises the Map from a ``Maps`` with ``data_origin='file'``."""
 
-        self.header = self.maps.data[self.property.name].header
+        header = maps.data[prop.name].header
 
-        if self.channel is not None:
-            channel_idx = self.property.channels.index(self.channel)
-            self.value = self.maps.data[self.property.name].data[channel_idx]
-            if self.maps_property.ivar:
-                self.ivar = self.maps.data[self.property.name + '_ivar'].data[channel_idx]
-            if self.maps_property.mask:
-                self.mask = self.maps.data[self.property.name + '_mask'].data[channel_idx]
+        if prop.channel is not None:
+            channel_idx = prop.channels.index(prop.channel)
+            value = maps.data[prop.name].data[channel_idx]
+            if prop.ivar:
+                ivar = maps.data[prop.name + '_ivar'].data[channel_idx]
+            if prop.mask:
+                mask = maps.data[prop.name + '_mask'].data[channel_idx]
         else:
-            self.value = self.maps.data[self.property.name].data
-            if self.maps_property.ivar:
-                self.ivar = self.maps.data[self.property.name + '_ivar'].data
-            if self.maps_property.mask:
-                self.mask = self.maps.data[self.property.name + '_mask'].data
+            value = maps.data[prop.name].data
+            if prop.ivar:
+                ivar = maps.data[prop.name + '_ivar'].data
+            if prop.mask:
+                mask = maps.data[prop.name + '_mask'].data
 
-        return
+        return value, ivar, mask, header
 
-    def _load_map_from_db(self):
+    @staticmethod
+    def _get_from_db(maps, prop):
         """Initialises the Map from a ``Maps`` with ``data_origin='db'``."""
 
         mdb = marvin.marvindb
@@ -151,58 +183,61 @@ class Map(object):
         if sqlalchemy is None:
             raise marvin.core.exceptions.MarvinError('sqlalchemy required to access the local DB.')
 
-        if version.StrictVersion(self.maps._dapver) <= version.StrictVersion('1.1.1'):
+        if version.StrictVersion(maps._dapver) <= version.StrictVersion('1.1.1'):
             table = mdb.dapdb.SpaxelProp
         else:
             table = mdb.dapdb.SpaxelProp5
 
-        fullname_value = self.property.db_column()
+        fullname_value = prop.db_column()
         value = mdb.session.query(getattr(table, fullname_value)).filter(
-            table.file_pk == self.maps.data.pk).order_by(table.spaxel_index).all()
-        self.value = np.array(value).reshape(self.shape).T
+            table.file_pk == maps.data.pk).order_by(table.spaxel_index).all()
+        value = np.array(value).reshape(maps.shape).T
 
-        if self.property.ivar:
-            fullname_ivar = self.property.db_column(ext='ivar')
+        if prop.ivar:
+            fullname_ivar = prop.db_column(ext='ivar')
             ivar = mdb.session.query(getattr(table, fullname_ivar)).filter(
-                table.file_pk == self.maps.data.pk).order_by(table.spaxel_index).all()
-            self.ivar = np.array(ivar).reshape(self.shape).T
+                table.file_pk == maps.data.pk).order_by(table.spaxel_index).all()
+            ivar = np.array(ivar).reshape(maps.shape).T
 
-        if self.property.mask:
-            fullname_mask = self.property.db_column(ext='mask')
+        if prop.mask:
+            fullname_mask = prop.db_column(ext='mask')
             mask = mdb.session.query(getattr(table, fullname_mask)).filter(
-                table.file_pk == self.maps.data.pk).order_by(table.spaxel_index).all()
-            self.mask = np.array(mask).reshape(self.shape).T
+                table.file_pk == maps.data.pk).order_by(table.spaxel_index).all()
+            mask = np.array(mask).reshape(maps.shape).T
 
         # Gets the header
-        hdus = self.maps.data.hdus
+        hdus = maps.data.hdus
         header_dict = None
         for hdu in hdus:
-            if self.property.name.upper() == hdu.extname.name.upper():
+            if prop.name.upper() == hdu.extname.name.upper():
                 header_dict = hdu.header_to_dict()
                 break
 
         if not header_dict:
             warnings.warn('cannot find the header for property {0}.'
-                          .format(self.property.name),
+                          .format(prop.name),
                           marvin.core.exceptions.MarvinUserWarning)
         else:
-            self.header = fits.Header(header_dict)
+            header = fits.Header(header_dict)
 
-    def _load_map_from_api(self):
+        return value, ivar, mask, header
+
+    @staticmethod
+    def _get_from_api(maps, prop):
         """Initialises the Map from a ``Maps`` with ``data_origin='api'``."""
 
         url = marvin.config.urlmap['api']['getmap']['url']
 
         url_full = url.format(
-            **{'name': self.maps.plateifu,
-               'property_name': self.property.name,
-               'channel': self.channel,
-               'bintype': self.maps.bintype,
-               'template': self.maps.template})
+            **{'name': maps.plateifu,
+               'property_name': prop.name,
+               'channel': prop.channel,
+               'bintype': maps.bintype.name,
+               'template': maps.template.name})
 
         try:
             response = marvin.api.api.Interaction(url_full,
-                                                  params={'release': self.maps._release})
+                                                  params={'release': maps._release})
         except Exception as ee:
             raise marvin.core.exceptions.MarvinError(
                 'found a problem when getting the map: {0}'.format(str(ee)))
@@ -213,12 +248,12 @@ class Map(object):
             raise marvin.core.exceptions.MarvinError(
                 'something went wrong. Error is: {0}'.format(response.results['error']))
 
-        self.value = np.array(data['value'])
-        self.ivar = np.array(data['ivar']) if data['ivar'] is not None else None
-        self.mask = np.array(data['mask']) if data['mask'] is not None else None
-        self.header = fits.Header(data['header'])
+        value = np.array(data['value'])
+        ivar = np.array(data['ivar']) if data['ivar'] is not None else None
+        mask = np.array(data['mask']) if data['mask'] is not None else None
+        header = fits.Header(data['header'])
 
-        return
+        return value, ivar, mask, header
 
     def save(self, path, overwrite=False):
         """Pickles the map to a file.
