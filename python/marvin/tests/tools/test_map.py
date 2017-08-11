156c9b7dc6336656a4d9e3a691e1d58284b141a6
Brett Andrews <brett.h.andrews@gmail.com>
2017-08-10 20:40:40 -0400

    PEP8

--- python/marvin/tests/tools/test_map.py
+++ python/marvin/tests/tools/test_map.py
@@ -75,7 +75,100 @@ class TestMap(object):
         map_restored = Map.restore(str(fout), delete=True)
         assert tuple(map_.shape) == tuple(map_restored.shape)
 
-<<<<<<< HEAD
+    @pytest.mark.parametrize('property_name, channel',
+                             [('emline_gflux', 'ha_6564'),
+                              ('stellar_vel', None)])
+    def test_deepcopy(self, galaxy, property_name, channel):
+        maps = Maps(plateifu=galaxy.plateifu)
+        map1 = maps.getMap(property_name=property_name, channel=channel)
+        map2 = copy.deepcopy(map1)
+
+        for attr in vars(map1):
+            if not attr.startswith('_'):
+                value = getattr(map1, attr)
+                value2 = getattr(map2, attr)
+
+                if isinstance(value, np.ndarray):
+                    assert np.isclose(value, value2).all()
+
+                elif isinstance(value, np.ma.core.MaskedArray):
+                    assert (np.isclose(value.data, value2.data).all() and
+                            (value.mask == value2.mask).all())
+
+                elif isinstance(value, Maps):
+                    pass
+
+                else:
+                    assert value == value2, attr
+
+    @pytest.mark.parametrize('property1, channel1, property2, channel2',
+                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
+                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
+    def test_add_maps(self, galaxy, property1, channel1, property2, channel2):
+        maps = Maps(plateifu=galaxy.plateifu)
+        map1 = maps.getMap(property_name=property1, channel=channel1)
+        map2 = maps.getMap(property_name=property2, channel=channel2)
+        map12 = map1 + map2
+
+        assert pytest.approx(map12.value == map1.value + map2.value)
+        assert pytest.approx(map12.ivar == map1._add_ivar(map1.ivar, map2.ivar))
+        assert pytest.approx(map12.mask == map1.mask & map2.mask)
+
+        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '+')
+        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '+')
+
+    @pytest.mark.parametrize('property1, channel1, property2, channel2',
+                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
+                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
+    def test_subtract_maps(self, galaxy, property1, channel1, property2, channel2):
+        maps = Maps(plateifu=galaxy.plateifu)
+        map1 = maps.getMap(property_name=property1, channel=channel1)
+        map2 = maps.getMap(property_name=property2, channel=channel2)
+        map12 = map1 - map2
+
+        assert pytest.approx(map12.value == map1.value - map2.value)
+        assert pytest.approx(map12.ivar == map1._add_ivar(map1.ivar, map2.ivar))
+        assert pytest.approx(map12.mask == map1.mask & map2.mask)
+
+        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '-')
+        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '-')
+
+    @pytest.mark.parametrize('property1, channel1, property2, channel2',
+                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
+                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
+    def test_multiply_maps(self, galaxy, property1, channel1, property2, channel2):
+        maps = Maps(plateifu=galaxy.plateifu)
+        map1 = maps.getMap(property_name=property1, channel=channel1)
+        map2 = maps.getMap(property_name=property2, channel=channel2)
+        map12 = map1 * map2
+
+        assert pytest.approx(map12.value == map1.value * map2.value)
+        assert pytest.approx(map12.ivar == map1._mul_ivar(map1.ivar, map2.ivar, map1.value,
+                                                          map2.value, map12.value))
+        assert pytest.approx(map12.mask == map1.mask & map2.mask)
+
+        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '*')
+        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '*')
+
+    @pytest.mark.parametrize('property1, channel1, property2, channel2',
+                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
+                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
+    def test_divide_maps(self, galaxy, property1, channel1, property2, channel2):
+        maps = Maps(plateifu=galaxy.plateifu)
+        map1 = maps.getMap(property_name=property1, channel=channel1)
+        map2 = maps.getMap(property_name=property2, channel=channel2)
+        map12 = map1 / map2
+
+        with np.errstate(divide='ignore', invalid='ignore'):
+            assert pytest.approx(map12.value == map1.value / map2.value)
+
+        assert pytest.approx(map12.ivar == map1._mul_ivar(map1.ivar, map2.ivar, map1.value,
+                                                          map2.value, map12.value))
+        assert pytest.approx(map12.mask == map1.mask & map2.mask)
+
+        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '/')
+        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '/')
+
     def test_getMap_invalid_property(self, galaxy):
         maps = Maps(plateifu=galaxy.plateifu)
         with pytest.raises(MarvinError) as ee:
@@ -95,7 +188,6 @@ class TestMap(object):
         stsig = maps['stellar_sigma']
         stsigcorr = maps['stellar_sigmacorr']
         stsig_physical = (stsig**2 - stsigcorr**2)**0.5
-        
         assert stsig_physical == stsig.inst_sigma_correction()
 
     def test_stellar_sigma_correction_invalid_property(self, galaxy):
@@ -107,103 +199,3 @@ class TestMap(object):
 
         assert ('Cannot correct {0}_{1} '.format(ha.property_name, ha.channel) +
                 'for instrumental broadening.') in str(ee.value)
-
-
-
-
-=======
-    @pytest.mark.parametrize('property_name, channel',
-                             [('emline_gflux', 'ha_6564'),
-                              ('stellar_vel', None)])
-    def test_deepcopy(self, galaxy, property_name, channel):
-        maps = Maps(plateifu=galaxy.plateifu)
-        map1 = maps.getMap(property_name=property_name, channel=channel)
-        map2 = copy.deepcopy(map1)
-
-        for attr in vars(map1):
-            if not attr.startswith('_'):
-                value = getattr(map1, attr)
-                value2 = getattr(map2, attr)
-                
-                if isinstance(value, np.ndarray):
-                    assert np.isclose(value, value2).all()
-                
-                elif isinstance(value, np.ma.core.MaskedArray):
-                    assert (np.isclose(value.data, value2.data).all() and
-                            (value.mask == value2.mask).all())
-                
-                elif isinstance(value, Maps):
-                    pass
-                
-                else:
-                    assert value == value2, attr
-
-    @pytest.mark.parametrize('property1, channel1, property2, channel2',
-                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
-                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
-    def test_add_maps(self, galaxy, property1, channel1, property2, channel2):
-        maps = Maps(plateifu=galaxy.plateifu)
-        map1 = maps.getMap(property_name=property1, channel=channel1)
-        map2 = maps.getMap(property_name=property2, channel=channel2)
-        map12 = map1 + map2
-        
-        assert pytest.approx(map12.value == map1.value + map2.value)
-        assert pytest.approx(map12.ivar == map1._add_ivar(map1.ivar, map2.ivar))
-        assert pytest.approx(map12.mask == map1.mask & map2.mask)
-        
-        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '+')
-        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '+')
-
-
-    @pytest.mark.parametrize('property1, channel1, property2, channel2',
-                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
-                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
-    def test_subtract_maps(self, galaxy, property1, channel1, property2, channel2):
-        maps = Maps(plateifu=galaxy.plateifu)
-        map1 = maps.getMap(property_name=property1, channel=channel1)
-        map2 = maps.getMap(property_name=property2, channel=channel2)
-        map12 = map1 - map2
-        
-        assert pytest.approx(map12.value == map1.value - map2.value)
-        assert pytest.approx(map12.ivar == map1._add_ivar(map1.ivar, map2.ivar))
-        assert pytest.approx(map12.mask == map1.mask & map2.mask)
-        
-        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '-')
-        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '-')
-
-    @pytest.mark.parametrize('property1, channel1, property2, channel2',
-                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
-                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
-    def test_multiply_maps(self, galaxy, property1, channel1, property2, channel2):
-        maps = Maps(plateifu=galaxy.plateifu)
-        map1 = maps.getMap(property_name=property1, channel=channel1)
-        map2 = maps.getMap(property_name=property2, channel=channel2)
-        map12 = map1 * map2
-        
-        assert pytest.approx(map12.value == map1.value * map2.value)
-        assert pytest.approx(map12.ivar == map1._mul_ivar(map1.ivar, map2.ivar, map1.value,
-                                                          map2.value, map12.value))
-        assert pytest.approx(map12.mask == map1.mask & map2.mask)
-        
-        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '*')
-        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '*')
-
-    @pytest.mark.parametrize('property1, channel1, property2, channel2',
-                             [('emline_gflux', 'ha_6564', 'emline_gflux', 'nii_6585'),
-                              ('emline_gvel', 'ha_6564', 'stellar_vel', None)])
-    def test_divide_maps(self, galaxy, property1, channel1, property2, channel2):
-        maps = Maps(plateifu=galaxy.plateifu)
-        map1 = maps.getMap(property_name=property1, channel=channel1)
-        map2 = maps.getMap(property_name=property2, channel=channel2)
-        map12 = map1 / map2
-        
-        with np.errstate(divide='ignore', invalid='ignore'):
-            assert pytest.approx(map12.value == map1.value / map2.value)
-
-        assert pytest.approx(map12.ivar == map1._mul_ivar(map1.ivar, map2.ivar, map1.value,
-                                                          map2.value, map12.value))
-        assert pytest.approx(map12.mask == map1.mask & map2.mask)
-        
-        assert map12.property_name == map1._combine_names(map1.property_name, map2.property_name, '/')
-        assert map12.channel == map1._combine_names(map1.channel, map2.channel, '/')
->>>>>>> f389699af776be8bbd3edfbfd3a0c9fd38aa6344
