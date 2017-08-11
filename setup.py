84bf2b67dfdd843f03a5c9de87cdf93cfc3e574f
Brian Cherinka <havok2063@hotmail.com>
2017-08-11 15:40:35 -0400

    testing out bumpversion

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # @Author: Brian Cherinka
 # @Date:   2016-10-19 17:36:00
 # @Last modified by:   Brian Cherinka
-# @Last Modified time: 2017-08-11 15:31:41
+# @Last Modified time: 2017-08-11 15:35:55
 #
 # This is the Marvin setup
 #
