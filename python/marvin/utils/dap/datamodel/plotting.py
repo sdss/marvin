b6dbd8ff104c20b459d10623eb2cfcb707c5db26
Brett Andrews <brett.h.andrews@gmail.com>
2017-08-29 11:25:50 -0400

    removes hardcoding of default bit values

--- python/marvin/utils/dap/datamodel/plotting.py
+++ python/marvin/utils/dap/datamodel/plotting.py
@@ -54,11 +54,8 @@ def get_default_mapset(dapver=None):
 def get_default_plot_params(dapver=None):
     """Returns default map plotting parameters."""
 
-    bitmasks = {'1.1.1': {'badData': {'doNotUse': 0}},
-                '2.0.2': {'nocov': 0,
-                          'badData': {'unreliable': 5,
-                                      'doNotUse': 30}
-                          }
+    bitmasks = {'1.1.1': ['DONOTUSE'],
+                '2.0.2': ['NOCOV', 'UNRELIABLE', 'DONOTUSE']
                 }
 
     plot_defaults = {
