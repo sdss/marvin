8c3c807ce54358657c4a528dfb7126c942a2c51b
Brett Andrews <brett.h.andrews@gmail.com>
2017-11-14 10:57:01 -0500

    loads bitmasks into datamodel

--- python/marvin/utils/datamodel/dap/base.py
+++ python/marvin/utils/datamodel/dap/base.py
@@ -36,7 +36,7 @@ class DAPDataModel(object):
 
     def __init__(self, release, bintypes=[], templates=[], properties=[], models=[],
                  default_template=None, default_bintype=None, property_table=None,
-                 default_binid=None, aliases=[]):
+                 default_binid=None, aliases=[], bitmasks={}):
 
         self.release = release
         self.bintypes = bintypes
@@ -46,6 +46,8 @@ class DAPDataModel(object):
 
         self.properties = PropertyList(properties, parent=self)
         self.models = ModelList(models, parent=self)
+
+        self.bitmasks = bitmasks
 
         self.property_table = property_table
 
