ecba411ca8d46561613bdd07b1dcddb694f06c70
Brett Andrews <brett.h.andrews@gmail.com>
2017-11-21 12:33:54 -0500

    replaces db_table with model

--- python/marvin/tools/maps.py
+++ python/marvin/tools/maps.py
@@ -416,7 +416,7 @@ class Maps(MarvinToolsClass, NSAMixIn, DAPallMixIn):
 
                     elif self.data_origin == 'db':
 
-                        table = getattr(mdb.dapdb, dm.db_table)
+                        table = getattr(mdb.dapdb, dm.model)
 
                         if table not in _db_rows:
                             _db_rows[table] = mdb.session.query(table).filter(
