a148ae0610d3bfa1f019d211d45a516ad14b7dac
Brett Andrews <brett.h.andrews@gmail.com>
2017-11-21 17:09:39 -0500

    fixes EnhancedMap pixmask

--- python/marvin/tools/quantities/map.py
+++ python/marvin/tools/quantities/map.py
@@ -545,6 +545,6 @@ class EnhancedMap(Map):
         See :ref:`marvin-utils-maskbit` for documentation and
         :meth:`marvin.utils.general.maskbit.Maskbit` for API reference.
         """
-        pixmask = self._datamodel.bitmasks['MANGA_DAPPIXMASK']
+        pixmask = self._datamodel.parent.bitmasks['MANGA_DAPPIXMASK']
         pixmask.mask = self.mask if self.mask is not None else None
         return pixmask
