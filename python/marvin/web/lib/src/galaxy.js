de70c0e8c84d4297d35324305adccd7bf2f11dbd
Brian Cherinka <havok2063@hotmail.com>
2018-03-01 15:15:04 -0500

    adding oi_to_ha ratio as a parameter; fixed weird mapspec view div mangling; issues #334 and #319

--- python/marvin/web/lib/src/galaxy.js
+++ python/marvin/web/lib/src/galaxy.js
@@ -2,7 +2,7 @@
 * @Author: Brian Cherinka
 * @Date:   2016-04-13 16:49:00
 * @Last Modified by:   Brian Cherinka
-* @Last Modified time: 2018-02-26 11:37:15
+* @Last Modified time: 2018-03-01 14:47:51
 */
 
 //
@@ -48,6 +48,7 @@ class Galaxy {
         this.webspec = null;
         this.staticdiv = this.specdiv.find('#staticdiv');
         this.dynamicdiv = this.specdiv.find('#dynamicdiv');
+        this.maptab = $('#maptab');
         // toggle elements
         this.togglediv = $('#toggleinteract');
         this.toggleload = $('#toggle-load');
@@ -82,6 +83,7 @@ class Galaxy {
         //this.checkToggle();
 
         //Event Handlers
+        this.maptab.on('click', this, this.resizeSpecView); // this event fires when a user clicks the MapSpec View Tab
         this.dapmapsbut.on('click', this, this.getDapMaps); // this event fires when a user clicks the GetMaps button
         this.resetmapsbut.on('click', this, this.resetMaps); // this event fires when a user clicks the Maps Reset button
         this.togglediv.on('change', this, this.initDynamic); // this event fires when a user clicks the Spec/Map View Toggle
@@ -115,6 +117,16 @@ class Galaxy {
             this.plateifu = plateifu;
         }
         [this.plate, this.ifu] = this.plateifu.split('-');
+    }
+
+    // Resize the ouput MapSpec View when tab clicked
+    resizeSpecView(event) {
+        let _this = event.data;
+        // wait 10 milliseconds before resizing so divs will have the correct size
+        m.utils.window[0].setTimeout(function () {
+            _this.webspec.resize();
+            _this.olmap.map.updateSize();
+        }, 10);
     }
 
     // Initialize and Load a DyGraph spectrum
