08b93f883d8479184ba33247b268a87f8915a11d
José Sánchez-Gallego <gallegoj@uw.edu>
2018-07-06 17:47:22 -0700

    Improve docstrings and add get_data prototype method with docstring

--- python/marvin/contrib/vacs/base.py
+++ python/marvin/contrib/vacs/base.py
@@ -5,8 +5,8 @@
 #
 # @Author: Brian Cherinka
 # @Date:   2018-06-21 17:01:09
-# @Last modified by:   Brian Cherinka
-# @Last Modified time: 2018-06-22 12:08:48
+# @Last modified by: José Sánchez-Gallego
+# @Last Modified time: 2018-07-06 17:46:12
 
 from __future__ import absolute_import, division, print_function
 
@@ -22,7 +22,28 @@ import sdss_access.sync
 
 
 class VACMixIn(object):
-    """This mixin allows for integrating a VAC into Marvin."""
+    """MixIn  that allows VAC integration in Marvin.
+
+    This parent class provides common tools for downloading data using
+    sdss_access or directly from the sandbox. `~VACMixIn.get_vacs` returns a
+    container with properties pointinh to all the VACs that subclass from
+    `.VACMixIn`. In general, VACs can be added to a class in the following way:
+
+    .. code-block:: python
+
+        from marvin.contrib.vacs.base import VACMixIn
+
+        class Maps(MarvinToolsClass):
+
+            def __init__(self, *args, **kwargs):
+
+                ...
+
+                self.vacs = VACMixIn.get_vacs(self)
+
+    and then the VACs can be accessed as properties in ``my_map.vacs``.
+
+    """
 
     # The name of the VAC.
     name = None
@@ -38,7 +59,49 @@ class VACMixIn(object):
         else:
             self.rsync_access = sdss_access.sync.RsyncAccess()
 
-    def get_vacs(self, parent_object):
+    def get_data(self, parent_object):
+        """Returns VAC data that matches the `parent_object` target.
+
+        This method must be overridden in each subclass of `VACMixIn`. Details
+        will depend on the exact implementation and the type of VAC, but in
+        general each version of this method must:
+
+        * Check whether the VAC file exists locally.
+        * If it does not, download it using `~VACMixIn.download_vac` (and,
+          possibly, by setting the path using `~VACMixIn.set_sandbox_path`).
+        * Open the file using the appropriate library.
+        * Retrieve the VAC data matching ``parent_object``. Usually one will
+          use attributes in ``parent_object`` such as ``.mangaid`` or
+          ``.plate`` to perform the match.
+        * Return the VAC data in whatever format is appropriate.
+
+        """
+
+        pass
+
+    @staticmethod
+    def get_vacs(parent_object):
+        """Returns a container with all the VACs subclassing from `VACMixIn`.
+
+        Because this method loops over ``VACMixIn.__subclasses__()``, all the
+        class that inherit from `VACMixIn` and that must be included in the
+        container need to have been imported before calling
+        `~VACMixIn.get_vacs`.
+
+        Parameters
+        ----------
+        parent_object : object
+            The object to which the VACs are being attached. It will be passed
+            to `~VACMixIn.get_data` when the subclass of `VACMixIn` is
+            called.
+
+        Returns
+        -------
+        vac_container : object
+            An instance of a class that contains just a list of properties, one
+            for to each on of the VACs that subclass from `VACMixIn`.
+
+        """
 
         class VACContainer(object):
 
@@ -80,7 +143,7 @@ class VACMixIn(object):
         return paths[0]  # doing this for single files, may need to change
 
     def set_sandbox_path(self, relative_path):
-        """Downloads a file using its path relative to ``MANGA_SANDBOX``."""
+        """Downloads a file using its path relative to ``$MANGA_SANDBOX``."""
 
         self.rsync_access.templates[self.name] = os.path.join('$MANGA_SANDBOX', relative_path)
 
