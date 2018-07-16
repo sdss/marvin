1e5b0c0cfb4f24ab9fea1f70ed94e15f740de7ad
José Sánchez-Gallego <gallegoj@uw.edu>
2018-07-15 18:32:38 -0700

    Change to RTD template for now

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -91,6 +91,8 @@ release = marvin_version
 # Usually you set "language" from the command line for these cases.
 language = None
 
+highlight_language = 'default'
+
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 # today = ''
@@ -153,71 +155,10 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {
-#     # Navigation bar title. (Default: ``project`` value)
-#     'navbar_title': "Marvin Docs",
 
-#     # Tab name for entire site. (Default: "Site")
-#     'navbar_site_name': "Site",
-
-#     # A list of tuples containing pages or urls to link to.
-#     # Valid tuples should be in the following forms:
-#     #    (name, page)                 # a link to a page
-#     #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
-#     #    (name, "http://example.com", True) # arbitrary absolute url
-#     # Note the "1" or "True" value above as the third argument to indicate
-#     # an arbitrary url.
-#     'navbar_links': [
-#         ("Go to Marvin &raquo;", "https://sas.sdss.org/marvin2/", True),
-#         ("Cheatsheet", "cheatsheet"),
-#         ("Tutorials", "tutorials"),
-#         ("New Issue", "https://github.com/sdss/marvin/issues/new", True)
-#     ],
-
-#     # Render the next and previous page links in navbar. (Default: true)
-#     'navbar_sidebarrel': False,
-
-#     # Render the current pages TOC in the navbar. (Default: true)
-#     'navbar_pagenav': True,
-
-#     # Tab name for the current pages TOC. (Default: "Page")
-#     'navbar_pagenav_name': "Page",
-
-#     # Global TOC depth for "site" navbar tab. (Default: 1)
-#     # Switching to -1 shows all levels.
-#     'globaltoc_depth': 2,
-
-#     # Include hidden TOCs in Site navbar?
-#     #
-#     # Note: If this is "false", you cannot have mixed ``:hidden:`` and
-#     # non-hidden ``toctree`` directives in the same page, or else the build
-#     # will break.
-#     #
-#     # Values: "true" (default) or "false"
-#     'globaltoc_includehidden': "true",
-
-#     # HTML navbar class (Default: "navbar") to attach to <div> element.
-#     # For black navbar, do "navbar navbar-inverse"
-#     'navbar_class': "navbar",
-
-#     # Fix navigation bar to top of page?
-#     # Values: "true" (default) or "false"
-#     'navbar_fixed_top': "true",
-
-#     # Location of link to source.
-#     # Options are "nav" (default), "footer" or anything else to exclude.
-#     'source_link_position': "nav",
-
-#     # Bootswatch (http://bootswatch.com/) theme.
-#     #
-#     # Options are nothing (default) or the name of a valid theme
-#     # such as "amelia" or "cosmo".
-#     'bootswatch_theme': "flatly",
-
-#     # Choose Bootstrap version.
-#     # Values: "3" (default) or "2" (in quotes)
-#     'bootstrap_version': "3",
-# }
+html_theme_options = {
+    'collapse_navigation': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
@@ -258,7 +199,7 @@ html_static_path = ['_static']
 # html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {'**': ['localtoc.html']}
+# html_sidebars = {'**': ['localtoc.html']}
 html_sidebars = {'**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
@@ -413,3 +354,4 @@ GoogleDocstring._parse = patched_parse
 # -- Use the custom css file in readthedocs --
 def setup(app):
     app.add_stylesheet('custom.css')
+    app.add_javascript('copybutton.js')
