71a6cb883f1bd60140a1e30b77fcc6ba86ac1720
Brian Cherinka <havok2063@hotmail.com>
2018-07-18 16:39:39 -0400

    back-tweaking mpl7 datamodel

--- python/marvin/utils/datamodel/dap/MPL7.py
+++ python/marvin/utils/datamodel/dap/MPL7.py
@@ -6,7 +6,7 @@
 # @Author: Brian Cherinka
 # @Date:   2018-04-04 23:07:41
 # @Last modified by:   Brian Cherinka
-# @Last Modified time: 2018-04-10 15:29:08
+# @Last Modified time: 2018-07-18 16:38:02
 
 from __future__ import print_function, division, absolute_import
 
@@ -25,5 +25,4 @@ MPL7 = DAPDataModel('2.2.0', aliases=['MPL-7', 'MPL7', 'DR15'],
                     default_bintype='HYB10',
                     default_template='GAU-MILESHC',
                     property_table='SpaxelProp7',
-                    default_binid=copy.deepcopy(binid_properties[0]),
-                    default_mapmask=['NOCOV', 'UNRELIABLE', 'DONOTUSE'])
+                    default_binid=copy.deepcopy(binid_properties[0]))
