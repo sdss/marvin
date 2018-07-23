0fb8d6dd62a070173b2fe69d34d6d8069a99df5e
José Sánchez-Gallego <gallegoj@uw.edu>
2018-07-23 01:07:44 -0700

    Provide access to RSS from marvin.tools

--- python/marvin/tools/__init__.py
+++ python/marvin/tools/__init__.py
@@ -1,6 +1,9 @@
+# flake8: noqa
+
 from . import cube, maps, modelcube, spaxel
 from .cube import Cube
 from .maps import Maps
 from .modelcube import ModelCube
 from .plate import Plate
+from .rss import RSS
 from .spaxel import Bin, Spaxel
