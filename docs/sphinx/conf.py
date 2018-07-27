1b734c41d09339427ec3b2d3b14942503960a603
José Sánchez-Gallego <gallegoj@uw.edu>
2018-07-27 11:27:05 -0700

    Remove h flag for tar

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -379,7 +379,7 @@ def get_test_data():
     os.chdir(os.path.expanduser('~/'))
 
     subprocess.run(['wget', url, os.path.expanduser('~/')])
-    ret = subprocess.run(['tar', 'xhvf', 'sas_rtd.tar.bz2'])
+    ret = subprocess.run(['tar', 'xvf', 'sas_rtd.tar.bz2'])
 
     assert ret.returncode == 0, 'failed to download or uncompress the test data.'
 
