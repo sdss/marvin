a7c1c41b87ce6b1cba6c3475082e83221155628b
Brian Cherinka <havok2063@hotmail.com>
2018-08-06 15:31:03 -0400

    fixing broken tests for stellar_sigmacorr due to string message change

--- python/marvin/tests/tools/test_map.py
+++ python/marvin/tests/tools/test_map.py
@@ -199,6 +199,7 @@ class TestMap(object):
         assert spaxel is not None
         assert spaxel.x == 10 and spaxel.y == 10
 
+    @marvin_test_if(mark='skip', galaxy=dict(release=['MPL-6']))
     def test_stellar_sigma_values(self, maps, galaxy):
         ''' Assert values for stellar_sigma and stellar_sigmacorr are different (issue #411) '''
 
@@ -212,6 +213,12 @@ class TestMap(object):
         scvalue = galaxy.dap['stellar_sigmacorr'][galaxy.bintype.name]
         assert ssvalue == pytest.approx(ss[x, y].value, 1e-4)
         assert scvalue == pytest.approx(sc[x, y].value, 1e-4)
+
+    @marvin_test_if(mark='include', galaxy=dict(release=['MPL-6']))
+    def test_stellar_sigma_mpl6(self, maps, galaxy):
+        with pytest.raises(MarvinError) as cm:
+            __ = maps.stellar_sigmacorr
+        assert 'stellar_sigmacorr is unreliable in MPL-6. Please use MPL-7.' in str(cm.value)
 
 
 class TestMapArith(object):
@@ -414,7 +421,7 @@ class TestMapArith(object):
         if galaxy.release == 'MPL-4':
             errmsg = 'Instrumental broadening correction not implemented for MPL-4.'
         elif galaxy.release == 'MPL-6':
-            errmsg = 'The stellar sigma corrections in MPL-6 are wrong. Please upgrade to MPL-7.'
+            errmsg = 'The stellar sigma corrections in MPL-6 are unreliable. Please use MPL-7.'
 
         with pytest.raises(MarvinError) as ee:
             stsig.inst_sigma_correction()
