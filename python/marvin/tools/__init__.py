9cd199f1978fe634adf19d1228b42c309ac4313c
José Sánchez-Gallego <gallegoj@uw.edu>
2018-08-11 11:05:37 -0700

    Disable import of Query and Results in __init__ untils problems are fixed

--- python/marvin/tools/__init__.py
+++ python/marvin/tools/__init__.py
@@ -2,11 +2,13 @@
 
 from . import cube, maps, modelcube, spaxel
 from .cube import Cube
+from .image import Image
 from .maps import Maps
 from .modelcube import ModelCube
 from .plate import Plate
 from .rss import RSS, RSSFiber
 from .spaxel import Bin, Spaxel
-from .image import Image
-from .query import Query
-from .results import Results
+
+
+# from .query import Query
+# from .results import Results
