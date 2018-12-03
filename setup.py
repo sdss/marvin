1190b481cdc42a0f49de101031f04feecc3bba60
Brian Cherinka <havok2063@hotmail.com>
2018-12-03 10:41:16 -0800

    Bump version: 2.3.0 → 2.3.1dev

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ install_requires = [line.strip().replace('==', '>=') for line in open(requiremen
 
 NAME = 'sdss-marvin'
 # do not use x.x.x-dev.  things complain.  instead use x.x.xdev
-VERSION = '2.3.0'
+VERSION = '2.3.1dev'
 RELEASE = 'dev' not in VERSION
 
 
