4f2744586d5eb21202667914a685c9d110359456
Brian Cherinka <havok2063@hotmail.com>
2019-08-23 11:54:01 -0400

    cleaning up gema vac with black; adding a file close; using log instead of print

--- python/marvin/contrib/vacs/mangagema.py
+++ python/marvin/contrib/vacs/mangagema.py
@@ -11,9 +11,8 @@
 from __future__ import print_function, division, absolute_import
 
 from astropy.io import fits
-import numpy as np
 import marvin.tools
-
+from marvin import log
 
 from .base import VACMixIn
 
@@ -34,38 +33,37 @@ class GEMAVAC(VACMixIn):
     # Required parameters
     name = 'mangagema'
     description = 'Returns GEMA table data'
-    version = {'MPL-7': '1.0.1',
-               'DR15': '1.0.1'}
+    version = {'MPL-7': '1.0.1', 'DR15': '1.0.1'}
 
     # optional Marvin Tools to attach your vac to
-    include = (marvin.tools.cube.Cube,
-               marvin.tools.maps.Maps,
-               marvin.tools.modelcube.ModelCube)
+    include = (marvin.tools.cube.Cube, marvin.tools.maps.Maps, marvin.tools.modelcube.ModelCube)
 
     # Required method
     def get_data(self, parent_object):
-        
+
         # Create a dictionary for GEMA VAC data
-        gemadata = {"completeness": "",
-                    "LSS_1_all": "",
-                    "LSS_1_002": "",
-                    "LSS_1_006": "",
-                    "LSS_1_010": "",
-                    "LSS_1_015": "",
-                    "LSS_5_all": "",
-                    "LSS_5_002": "",
-                    "LSS_5_006": "",
-                    "LSS_5_010": "",
-                    "LSS_5_015": "",
-                    "pairs": "",
-                    "groups": "",
-                    "overdensity": "",
-                    "LSS_tensor": ""}
-        
+        gemadata = {
+            "completeness": "",
+            "LSS_1_all": "",
+            "LSS_1_002": "",
+            "LSS_1_006": "",
+            "LSS_1_010": "",
+            "LSS_1_015": "",
+            "LSS_5_all": "",
+            "LSS_5_002": "",
+            "LSS_5_006": "",
+            "LSS_5_010": "",
+            "LSS_5_015": "",
+            "pairs": "",
+            "groups": "",
+            "overdensity": "",
+            "LSS_tensor": "",
+        }
+
         # get any parameters you need from the parent object
         mangaid = parent_object.mangaid
         release = parent_object.release
-        
+
         # define the variables to build a unique path to your VAC file
         path_params = {'ver': self.version[release]}
 
@@ -75,18 +73,20 @@ class GEMAVAC(VACMixIn):
         # download the vac from the SAS if it does not already exist locally
         if not gemapath:
             gemapath = self.download_vac('mangagema', path_params=path_params)
-        
+
         # opening tables in VAC file
         gemafile = fits.open(gemapath)
-        
+
         # Return selected line(s) for a mangaid galaxy
         # LSS parameters
         indata = mangaid in gemafile[1].data['mangaid']
         if not indata:
-            print("No LSS data exists for {0}".format(mangaid))
+            log.warning("No LSS data exists for {0}".format(mangaid))
         else:
-            print("LSS data exists for {0}".format(mangaid))
-            print("Warning: Do not use LSS parameters defined in volume with z < z_manga_galaxy")
+            log.info("LSS data exists for {0}".format(mangaid))
+            log.warning(
+                "Warning: Do not use LSS parameters defined in volume with z < z_manga_galaxy"
+            )
             indexLSS = gemafile[1].data['mangaid'] == mangaid
             gemadata["completeness"] = gemafile[1].data[indexLSS]
             gemadata["LSS_1_all"] = gemafile[2].data[indexLSS]
@@ -99,43 +99,46 @@ class GEMAVAC(VACMixIn):
             gemadata["LSS_5_006"] = gemafile[9].data[indexLSS]
             gemadata["LSS_5_010"] = gemafile[10].data[indexLSS]
             gemadata["LSS_5_015"] = gemafile[11].data[indexLSS]
-        
+
         # Local environment: close pair galaxies
         indata = mangaid in gemafile[12].data['mangaid']
         if not indata:
-            print("No pair data exists for {0}".format(mangaid))
+            log.warning("No pair data exists for {0}".format(mangaid))
         else:
-            print("Pair data exists for {0}".format(mangaid))
+            log.info("Pair data exists for {0}".format(mangaid))
             indexpair = gemafile[12].data['mangaid'] == mangaid
             gemadata["pairs"] = gemafile[12].data[indexpair]
-        
+
         # Local environment: group galaxies
         indata = mangaid in gemafile[13].data['mangaid']
         if not indata:
-            print("No group data exists for {0}".format(mangaid))
+            log.warning("No group data exists for {0}".format(mangaid))
         else:
-            print("Group data exists for {0}".format(mangaid))
+            log.info("Group data exists for {0}".format(mangaid))
             indexgroup = gemafile[13].data['mangaid'] == mangaid
             gemadata["groups"] = gemafile[13].data[indexgroup]
-        
+
         # LSS overdensity-corrected local density
         indata = mangaid in gemafile[14].data['mangaid']
         if not indata:
-            print("No overdensity data exists for {0}".format(mangaid))
+            log.warning("No overdensity data exists for {0}".format(mangaid))
         else:
-            print("Overdensity data exists for {0}".format(mangaid))
+            log.info("Overdensity data exists for {0}".format(mangaid))
             indexover = gemafile[14].data['mangaid'] == mangaid
             gemadata["overdensity"] = gemafile[14].data[indexover]
-        
-        # LSS structure 
+
+        # LSS structure
         indata = mangaid in gemafile[15].data['mangaid']
         if not indata:
-            print("No structure data exists for {0}".format(mangaid))
+            log.warning("No structure data exists for {0}".format(mangaid))
         else:
-            print("Structure data exists for {0}".format(mangaid))
+            log.info("Structure data exists for {0}".format(mangaid))
             indextensor = gemafile[15].data['mangaid'] == mangaid
             gemadata["LSS_tensor"] = gemafile[15].data[indextensor]
-        
+
         gema_data = gemadata
-        
+
+        # closing the FITS file
+        gemafile.close()
+
         return gema_data
