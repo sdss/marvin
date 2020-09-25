10f6eefe6f44719e5d07b3cf780c56bc456b934b
Brian Cherinka <havok2063@hotmail.com>
2020-09-25 14:43:16 -0400

    Bump version: 2.5.1dev → 2.5.1

--- python/marvin/__init__.py
+++ python/marvin/__init__.py
@@ -19,7 +19,7 @@ from collections import OrderedDict
 from astropy.wcs import FITSFixedWarning
 
 # Set the Marvin version
-__version__ = '2.5.1dev'
+__version__ = '2.5.1'
 
 # Does this so that the implicit module definitions in extern can happen.
 # time - 483 ms
