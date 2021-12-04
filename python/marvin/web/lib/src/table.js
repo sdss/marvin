8513797b3fed4e98babe6ed2dc91f8c195fe82c1
Brian Cherinka <havok2063@hotmail.com>
2021-12-04 11:31:18 -0500

    improving search table performance; adding export button

--- python/marvin/web/lib/src/table.js
+++ python/marvin/web/lib/src/table.js
@@ -17,6 +17,8 @@ class Table {
         // Event Handlers
         this.table.on('load-success.bs.table', this, this.setSuccessMsg);
         this.table.on('load-error.bs.table', this, this.setErrMsg);
+        $('#export-json').on('click', this, this.exportTable); 
+        $('#export-csv').on('click', this, this.exportTable); 
     }
 
     // Print
@@ -45,30 +47,51 @@ class Table {
             cols = this.makeColumns(data.columns);
         }
 
+        this.data = data;
+        this.initPageSize = 10;
         console.log(data);
         console.log('cols', cols);
+
         // init the Bootstrap table
         this.table.bootstrapTable({
             classes: 'table table-bordered table-condensed table-hover',
             toggle : 'table',
             toolbar: '#toolbar',
             pagination : true,
-            pageSize: 10,
+            pageSize: this.initPageSize,
             pageList : '[10, 20, 50]',
             sidePagination: 'server',
             method: 'post',
             contentType: "application/x-www-form-urlencoded",
-            data: data.rows,
+            data: data.rows.slice(0, this.initPageSize),
             totalRows: data.total,
             columns: cols,
-            url: url,
+            deferUrl: url,
             showColumns : true,
             showToggle : true,
+            queryParams: this.queryParams,
             sortName: 'mangaid',
             sortOrder: 'asc',
+            usePipeline: true,
             formatNoMatches: ()=>{ return "This table is empty..."; }
         });
     }
+
+    // send additional query parameters with each request
+    queryParams(params) {
+        this.table = $('#searchtable');
+        var options = this.table.bootstrapTable('getOptions');
+        console.log('table opts', options);
+        console.log('parmas', params);
+
+        // this is necessary to ensure the backend has the validated API parameters it needs
+        // the table pipeline extension renames some of the query parameters
+        if (options.usePipeline) {
+            params.order = params.sortOrder;
+            params.sort = params.sortName;
+        }
+        return params;
+      }
 
     // update the error div with a message
     updateMsg(msg) {
@@ -126,7 +149,7 @@ class Table {
         if (this.table === null) {
             this.setTable();
         }
-        this.table = $('#table');
+        this.table = $('#searchtable');
         // Get new columns
         let cols = results.columns;
         cols = [];
@@ -144,4 +167,42 @@ class Table {
         return results;
     }
 
+    // export the table data
+    exportTable(event) {
+        let _this = event.data;
+        let filetype = event.currentTarget.name;
+
+        let options = $('#searchtable').bootstrapTable('getOptions');
+        let url = options.deferUrl;
+
+        let params = {limit: options.totalRows, export: true, filetype: filetype,
+            offset: 0, sort: 'mangaid', order: 'asc'};
+
+        let form = {'body': JSON.stringify(params), 
+        'method':'POST', 'headers':{'Content-Type':'application/json'}};
+
+        // toggle on export displays
+        $('#export-btn').children('button').addClass('btn-warning');
+        $('#export-load').show();
+
+        // fetch the full query results
+        return fetch(url, form)
+        .then((response) => {return response.blob();})
+        .then((blob) => {
+            // convert blob into a file link and prompt to download
+            var url = window.URL.createObjectURL(blob);
+            var link = document.createElement('a');
+            document.body.appendChild(link);
+            link.style = "display: none";
+            link.href = url;
+            link.download = "marvin_table."+filetype;
+            link.click();
+        })
+        .then(() => {
+            // toggle off export displays
+            $('#export-btn').children('button').removeClass('btn-warning');
+            $('#export-load').hide();
+        });
+
+    }
 }
