5e072c6bfd02f675d37e4c181c48e481fd75da1c
Brian Cherinka <havok2063@hotmail.com>
2025-06-12 12:12:51 -0400

    Updates to docker build (#831)
    * adding docker dev files
    
    * updating gunicorn threads
    
    * fixing docker build issues, removing header login ui
    
    * adding docker build github action
    
    * tweak to action
    
    * test action context
    
    * test action context
    
    * test action context
    
    * updating changelog
    
    * Bump version: 2.8.2 → 2.8.3dev

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -3,6 +3,8 @@ import os
 socket_dir = os.getenv("MARVIN_SOCKET_DIR", '/tmp/marvin')
 bind = [f"unix:{socket_dir}/marvin.sock", "0.0.0.0:8000"]
 workers = 1
+worker_class = "gthread"
+threads = 8
 daemon = False
 errorlog = os.path.join(os.getenv("MARVIN_LOGS_DIR", '/tmp/marvin/logs'), 'marvin_app_error.log')
 accesslog = os.path.join(os.getenv("MARVIN_LOGS_DIR", '/tmp/marvin/logs'), 'marvin_app_access.log')
